9804751215c12d5d2cb69107651717b3689ec6c9
marcos-vas <marcosvaas@gmail.com>
2021-12-21 10:42:02 -0300

    feat: levantamento de colunas

---
+++ src/headers_keys.py
@@ -0,0 +1,36 @@
+CONTRACHEQUE = "contracheque"
+INDENIZACOES = "indenizacoes"
+
+HEADERS = {
+    CONTRACHEQUE: {
+        "Remuneração do Cargo Efetivo": "COL01",
+        "Outras Verbas Remuneratórias Legais/Judiciais": "COL02",
+        "Função de Confiança ou Cargo em Comissão": "COL03",
+        "13o. Salário": "COL04",
+        "Adicional de Férias": "COL05",
+        "Abono de Permanência": "COL06",
+        "Contribuição Previdenciária": "COL08",
+        "Imposto de Renda": "COL09",
+        "Retenção do Teto": "COL10",
+    },
+
+    INDENIZACOES: {
+        "Auxílio-Alimentação": "AUX_ALIMENTACAO",
+        "Auxílio-Saúde": "AUX_SAUDE",
+        "Auxílio-Moradia": "AUX_MORADIA",
+        "Auxílio-Transporte": "AUX_TRANSPORTE",
+        "Auxílio-Creche": "AUX_CRECHE",
+        "Ajuda de Custo": "AJUDA_CUSTO",
+        "Auxílio-Escola": "AUX_ESCOLA",
+        "Auxílio-Odontológico":"AUX_ODONTO",
+        "Outros Auxílios": "OUTROS_AUX",
+        "Abonos": "ABONOS",
+        "Adicionais": "ADICIONAIS",
+        "Comissões": "COMISSOES",
+        "Diferenças": "DIFERENCAS",
+        "Indenizações": "INDENIZACOES",
+        "Outras Remunerações": "OUTRAS_REMUNERACOES",
+        "Gratificações": "GRATIFICACOES",
+
+    },
+}
